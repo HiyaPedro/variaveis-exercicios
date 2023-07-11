d4fc9f274a970a7b174079c323b03c29b9dd4523
Peter <peterjcooper4@gmail.com>
2023-07-11 20:26:10 -0300

    exercisos de fixacao terminado

---
+++ exercicio2/script.js
@@ -0,0 +1,31 @@
+// criar 3 perguntas (boolean)
+
+//a. 3 perguntas para o usuário através do prompt
+
+/*
+const nome = prompt("Qual é o seu nome?")
+let idade = prompt("Qual é o seu idade?")
+let precença = prompt("Vai para a festa?")
+
+console.log(nome)
+console.log(idade)
+console.log(precença)
+*/
+
+const perguntaA = "Qual é o seu nome?"
+let perguntaB = "Qual é o seu idade?"
+let perguntaC = "Vai para a festa?"
+
+const respostaA = prompt(perguntaA)
+let respostaB = prompt(perguntaB)
+let respostaC = prompt(perguntaC)
+
+console.log(respostaA)
+console.log(respostaB)
+console.log(respostaC)
+
+
+
+
+
+
